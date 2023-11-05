719e209683013280633b24f62fe82b61da8464e2
baseballyama <xydybaseball@gmail.com>
2023-11-05 19:04:09 +0900

    refactor

--- packages/ai-craftsman/src/utils/git.ts
+++ packages/ai-craftsman/src/utils/git.ts
@@ -128,10 +128,8 @@ export const postReviewComment = async (
 export const getBaseRef = async () => {
   const ref = github.context.payload.pull_request?.["base"]?.ref;
   if (ref) return ref;
+  const { owner, repo } = getOwnerAndRepo();
   const pullNumber = github.context.payload.issue?.["pull_request"]?.number;
-  const owner = github.context.repo.owner;
-  const repo = github.context.repo.repo;
-
   const octokit = getOctokit();
   const { data: pullRequest } = await octokit.pulls.get({
     owner,
